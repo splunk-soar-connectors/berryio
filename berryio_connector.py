50faf07e1a50a3c307064a869d923d0ea1b1bfa1
Mishal <mishalp@splunk.com>
2021-11-16 10:52:09 +0530

    resolved review comments

--- berryio_connector.py
+++ berryio_connector.py
@@ -346,18 +346,43 @@ if __name__ == '__main__':
     python2.7 ./berryio.py /tmp/berryio.json
     """
 
-    # Imports
+    import argparse
     import sys
 
     import pudb
-
-    # Breakpoint at runtime
     pudb.set_trace()
 
-    # The first param is the input json file
-    with open(sys.argv[1]) as f:
+    argparser = argparse.ArgumentParser()
 
-        # Load the input json file
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    if args.username and args.password:
+        login_url = BaseConnector._get_phantom_base_url() + "login"
+        try:
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=False)
+            csrftoken = r.cookies['csrftoken']
+            data = {'username': args.username, 'password': args.password, 'csrfmiddlewaretoken': csrftoken}
+            headers = {'Cookie': 'csrftoken={0}'.format(csrftoken), 'Referer': login_url}
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            session_id = r2.cookies['sessionid']
+
+        except Exception as e:
+            print("Unable to get session id from the platform. Error: {0}".format(str(e)))
+            sys.exit(1)
+
+    if len(sys.argv) < 2:
+        print("No test json specified as input")
+        sys.exit(0)
+
+    with open(sys.argv[1]) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=' ' * 4))
@@ -365,13 +390,12 @@ if __name__ == '__main__':
         # Create the connector class object
         connector = BerryIOConnector()
 
-        # Se the member vars
         connector.print_progress_message = True
 
-        # Call BaseConnector::_handle_action(...) to kickoff action handling.
-        ret_val = connector._handle_action(json.dumps(in_json), None)
+        if session_id is not None:
+            in_json['user_session_token'] = session_id
 
-        # Dump the return value
-        print(ret_val)
+        ret_val = connector._handle_action(json.dumps(in_json), None)
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     sys.exit(0)
